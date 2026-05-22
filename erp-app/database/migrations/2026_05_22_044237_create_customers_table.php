d630cab63f3ef857745efe9bbd13e189094986f7
Tdrynn <tuderyan1936@gmail.com>
2026-05-22 13:31:01 +0800

    Create customer migration

--- erp-app/database/migrations/2026_05_22_044237_create_customers_table.php
+++ erp-app/database/migrations/2026_05_22_044237_create_customers_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string("customer_id")->unique();
             $table->string("name");
             $table->string("email")->nullable();
             $table->string("phone")->nullable();
